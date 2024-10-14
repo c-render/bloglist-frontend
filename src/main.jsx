8d2b3a6fa16fd46ec1b2ae413b766490edbd27b4
Carlo Scharnhorst <cscharnhorst@microsoft.com>
2024-10-14 10:49:22 +0200

    Exercise 7.13 redux separate store

--- src/main.jsx
+++ src/main.jsx
@@ -4,26 +4,7 @@ import App from './App'
 import { Provider } from 'react-redux'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import blogReducer from './reducers/blogReducer'
-import { configureStore } from '@reduxjs/toolkit'
-import notificationReducer from './reducers/notificationReducer'
-import userReducer from './reducers/userReducer'
-
-//const store = configureStore({
-//    reducer: {
-//        notification: notificationReducer
-//    }
-//})
-
-const store = configureStore({
-    reducer: {
-        notification: notificationReducer,
-        blogs: blogReducer,
-        user: userReducer
-    }
-})
-
-console.log('store:', store.getState())
+import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
